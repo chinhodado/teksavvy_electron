e9061fe1e9bfcc116fc8c20fc0a181ae56d0f04c
Trung Do <chin.bimbo@gmail.com>
2019-12-25 17:20:38 -0500

    Update to latest electron

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ function createWindow () {
   mainWindow = new BrowserWindow({
       width: 800,
       height: 600,
+      webPreferences: {
+          nodeIntegration: true
+      }
   })
 
     mainWindow.setMenuBarVisibility(false)
