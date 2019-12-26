481be01f9d9b7e163ed3a769cc3c6d9a420b7774
Trung Do <chin.bimbo@gmail.com>
2019-12-25 21:34:05 -0500

    Add F12 webtool and F5 reload

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,3 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+window.addEventListener("keydown", function (e) {
+    if (e.which === 123) { //F12
+        require('electron').remote.getCurrentWindow().webContents.openDevTools()
+    } else if (e.which === 116) { // F5
+        location.reload();
+    }
+});
